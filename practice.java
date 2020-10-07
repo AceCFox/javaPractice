c96ae8e2a521d3873d994088ebac881d4a238441
Ace Fox <afox09@gmail.com>
2020-10-07 13:44:32 -0500

    if -then

--- practice.java
+++ practice.java
@@ -1,7 +1,9 @@
 public class ConditionalPlayground {
     public static void main(String[] args) {
-      
-      
+      int benchMark = 52;
+      if (benchMark > 50){
+          System.out.println("benchmark passed!");
+      }
       
     }
   }
