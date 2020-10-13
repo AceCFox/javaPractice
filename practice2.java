a18dd0bd18799b867ef32220e2300431219ee8b7
Ace Fox <afox09@gmail.com>
2020-10-13 15:12:41 -0500

    practice 2

---
+++ practice2.java
@@ -0,0 +1,8 @@
+public class ConditionalPlayground {
+    public static void main(String[] args) {
+      double price = 30.34;
+      String capitalism = "That's expensive!";
+      System.out.println(capitalism + "  $" + price);
+
+    }
+  }
